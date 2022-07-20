ee730ff443096fe9c24734f2a6d8667110f9c52e
Joy879 <joywanjiru879@gmail.com>
2022-07-19 17:06:15 -0700

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,6 +25,7 @@ def get_locale():
     """Determines best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """
